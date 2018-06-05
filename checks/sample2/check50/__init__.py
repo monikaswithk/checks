1b8e997acd3c6923988820ebb29e470da1738f7e
Monika Richardson <monikasrichardson@gmail.com>
2018-06-05 23:27:23 +0000

    first check50

---
+++ checks/sample2/check50/__init__.py
@@ -0,0 +1,19 @@
+from check50 import *
+
+class Sample2(Checks):
+
+    @check()
+    def exists(self):
+
+        """sample2.py exists"""
+        self.require("2.py")
+
+
+
+    @check("exists")
+
+    def test_5_output(self):
+
+        """input of nothing yields output of 5"""
+
+        self.spawn("./python sample2.py").stdout("5\n", "5\n").exit(0)
