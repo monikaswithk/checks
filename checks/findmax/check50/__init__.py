935a355e1f27f06d876bdb9d33bcdc08eefbcfab
Monika Richardson <monikasrichardson@gmail.com>
2018-08-27 10:27:31 +0000

    adding findmax

---
+++ checks/findmax/check50/__init__.py
@@ -0,0 +1,32 @@
+from check50 import *
+
+
+from check50 import *
+
+
+class findMax(Checks):
+
+    @check()
+    def exists(self):
+        """findMax.py exists"""
+        self.require("findMax.py")
+
+    @check("exists")
+    def normalcase(self):
+        """normalcase"""
+        self.spawn("python findMax.py").stdin("28").stdin("1").stdin("5").stdin("9").stdin("2").stdin("65").stdin("7").stdin("4").stdin("63").stdin("17").stdout("\65\n", "65\n").exit(0)
+
+    @check("exists")
+    def allthedame(self):
+        """all the same"""
+        self.spawn("python findMax.py").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdin("31").stdout("\31\n", "31\n").exit(0)
+
+    @check("exists")
+    def negatives(self):
+        """allnegatives"""
+        self.spawn("python findMax.py").stdin("-31").stdin("-31").stdin("-34").stdin("-31").stdin("-31").stdin("-31").stdin("-31").stdin("-31").stdin("-30").stdin("-32").stdout("\-30\n", "-30\n").exit(0)
+
+    @check("exists")
+    def firstismax(self):
+        """firstismax"""
+        self.spawn("python findMax.py").stdin("29").stdin("2").stdin("2").stdin("1").stdin("-2").stdin("4").stdin("2").stdin("9").stdin("2").stdin("6").stdout("\29\n", "29\n").exit(0)
