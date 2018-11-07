44c366dd29c10961e3f5c080da4be19e1e1c60a1
Monika Richardson <monikasrichardson@gmail.com>
2018-11-07 07:09:25 +0000

    fixed initials more

--- checks/initialsmore/check50/__init__.py
+++ checks/initialsmore/check50/__init__.py
@@ -1,7 +1,36 @@
 less = __import__("check50").import_from("../initialsless")
 from less import *
 
-class InitialsMore(InitialsLess):
+class InitialsMore(Checks):
+
+    @check()
+    def exists(self):
+        """initials.py exists."""
+        self.require("initials.py")
+
+
+    @check("exists")
+    def uppercase(self):
+        """Outputs HLJ for Hailey Lynn James"""
+        self.spawn("python initials.py").stdin("Hailey Lynn James", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+
+    @check("exists")
+    def lowercase(self):
+        """Outputs HLJ for hailey lynn james"""
+        self.spawn("python initials.py").stdin("hailey lynn james", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+
+    @check("exists")
+    def mixed_case(self):
+        """Outputs HJ for hailey James"""
+        self.spawn("python initials.py").stdin("hailey James", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+
+    @check("exists")
+    def all_uppercase(self):
+        """Outputs B for BRIAN"""
+        self.spawn("python initials.py").stdin("BRIAN", prompt=False).stdout(match("B"), "B\n").exit(0)
+
+
+
 
     @check("exists")
     def space_between(self):
@@ -12,3 +41,6 @@ class InitialsMore(InitialsLess):
     def space_before_after(self):
         """Outputs HJ for     hailey James    """
         self.spawn("python initials.py").stdin("    hailey James    ", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+
+def match(initials):
+    return "^(.*\n)?{}\n".format(initials)
